084f3b8aa58e1e12993067c62095a280d624c652
DenysRudenko <heartbreathing19951014@gmail.com>
2023-10-15 22:41:44 +0100

    Solved Challenge2

--- Unit-2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Unit-2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float i = 0;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && i <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            i++;
+        }
+        if (i >= 0)
+        {
+            i -= Time.deltaTime;
         }
     }
 }
